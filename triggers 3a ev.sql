ca87f72d80d30249d55d72261dcb71b647e90ceb
pepejulian <161172067+notpepejulian@users.noreply.github.com>
2024-05-09 11:40:37 +0200

    Update triggers 3a ev.sql

--- triggers 3a ev.sql
+++ triggers 3a ev.sql
@@ -27,3 +27,40 @@ end;
 /
 
 
+
+
+create table T_SALDO(
+    saldo number(10,2)
+);
+
+
+create or replace trigger TRIGGERENTRADA
+after insert on compra
+for each row
+declare
+begin
+    -- Actualizamos el saldo restando el dinero gastado en la compra
+    update T_SALDO set saldo = nvl(saldo, 0) - (:new.cantidad * :new.precio_proveedor);
+    
+    -- Si no hay ninguna fila en T_SALDO, creamos una nueva fila con el saldo actualizado
+    if sql%notfound then
+        insert into T_SALDO values (:new.cantidad * :new.precio_proveedor * -1);
+    end if;
+end;
+/
+
+-- Creamos el trigger para la tabla venta (salida de productos)
+create or replace trigger TRIGGERSALIDA
+after insert on venta
+for each row
+declare
+begin
+    -- Actualizamos el saldo sumando el dinero obtenido por la venta
+    update T_SALDO set saldo = nvl(saldo, 0) + (:new.cantidad * :new.precio_unidad);
+    -- Si no hay ninguna fila en T_SALDO, creamos una nueva fila con el saldo actualizado
+    if sql%notfound then
+        insert into T_SALDO values (:new.cantidad * :new.precio_unidad);
+    end if;
+end;
+/
+
